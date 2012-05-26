9a8f24d3d8c22d6a18c3b690b44ac4e2d9efbd4a
Joshua Abbott <joshnabbott@gmail.com>
2012-05-26 13:52:00 -0700

    Styling page and post templates
    Moved originals into /old

--- source/templates/page.php
+++ source/templates/page.php
@@ -5,11 +5,7 @@
     </section>
 
     <div id="content-primary" class="row-fluid">
-      <section class="span3">
-        This is a sidebar
-      </section>
-
-      <section class="well span9">
+      <section class="well span12">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </section>
     </div><!-- end content-primary -->
--- source/templates/single.php
+++ source/templates/single.php
@@ -1,30 +1,42 @@
 <?php get_header(); ?>
-	<?php if ( have_posts() ) : ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<h1><?php the_title(); ?></h1>
-				<div>
-					<?php printf( __( 'by %s on', 'phenom' ), get_the_author() ); ?> <?php the_date(); ?>
-				</div>
-				<div class="entry">
-					<?php if ( has_post_thumbnail() ) {
-						the_post_thumbnail();
-					} ?>
-					<?php the_content(); ?>
-					<?php edit_post_link( __( 'Edit this', 'phenom' ), '<p>', '</p>' ); ?>
-					<?php wp_link_pages(); ?>
-				</div><!--end entry-->
-				<div class="post-footer clear">
-					<div class="tags">
-						<?php the_tags( __( 'Tags: ', 'phenom' ), ', ', '' ); ?>
-					</div>
-					<div class="cats">
-						<?php printf( __( 'From: %s', 'phenom' ), get_the_category_list( ', ' ) ); ?>
-					</div>
-				</div><!--end post footer-->
-			</div><!--end post-->
-		<?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
-		<?php comments_template( '', true ); ?>
-	<?php endif; ?>
-<?php get_sidebar(); ?>
+    <section id="content-top" class="row-fluid">
+      <h3>/Blog</h3>
+    </section>
+
+    <div id="content-primary" class="row-fluid">
+      <section class="span3">
+        <?php get_sidebar(); ?>
+      </section>
+
+      <section class="well span9">
+        <?php if ( have_posts() ) : ?>
+          <?php while ( have_posts() ) : the_post(); ?>
+            <article <?php post_class(); ?>>
+              <h2><?php the_title(); ?></h2>
+              <small><?php printf( __( 'by %s on', 'phenom' ), get_the_author() ); ?> <?php the_date(); ?></small>
+
+              <div class="row-fluid">
+                <?php if ( has_post_thumbnail() ) : ?>
+                  <div class="thumbnail pull-left"><?php the_post_thumbnail(); ?></div>
+                <?php endif; ?>
+                <?php the_content(); ?>
+                <?php wp_link_pages(); ?>
+              </div>
+
+              <div class="row-fluid">
+                <div>
+                  <?php the_tags( __( 'Tags: ', 'phenom' ), ', ', '' ); ?>
+                </div>
+                <div class="pull-right hide">
+                  <?php printf( __( 'From: %s', 'phenom' ), get_the_category_list( ', ' ) ); ?>
+                </div>
+              </div>
+            </article>
+          <?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
+          <?php comments_template( '', true ); ?>
+        <?php endif; ?>
+      </section>
+    </div><!-- end content-primary -->
+
 <?php get_footer(); ?>
+
