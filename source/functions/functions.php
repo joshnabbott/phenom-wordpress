3e01310a642805b2ac0d9e594eeea5030c04dcad
Joshua Abbott <joshnabbott@gmail.com>
2012-06-06 14:54:11 -0700

    Rename functions in functions.

--- source/functions/functions.php
+++ source/functions/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'phenom_enqueue_scripts' );
-add_action( 'init', 'register_my_menus' );
+add_action( 'init', 'register_menus' );
 
-function register_my_menus() {
+function register_menus() {
   register_nav_menus(
     array(
       'primary-nav' => __( 'Primary Nav' ),
