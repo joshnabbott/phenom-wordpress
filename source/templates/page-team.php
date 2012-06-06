044ecc32d3bba18f9da4726bb7c32399b0009b20
Joshua Abbott <joshnabbott@gmail.com>
2012-06-06 14:51:17 -0700

    Adding team stats and photos.

--- source/templates/page-team.php
+++ source/templates/page-team.php
@@ -13,13 +13,246 @@
           </div>
           <dl>
             <dt>Name</dd>
-            <dd>Kenny Leverich</dd>
-            <dt>Age</dd>
-            <dd>22</dd>
-            <dt>Years competing</dd>
-            <dd>10</dd>
-            <dt>Favorite workout</dd>
-            <dd>Fran</dd>
+            <dd>Kenny Leverich<br/>"Soak up some rays, mess up some waves."</dd>
+            <dt>PRs</dd>
+            <dd>
+              OHS - 315
+              <br/>
+              C&amp;J - 315
+              <br/>
+              BS - 450
+              <br/>
+              Clean - 345
+              <br/>
+              Snatch - 255
+              <br/>
+              Press - 215
+              <br/>
+              DL - 515
+              <br/>
+              BJ - 58
+              <br/>
+              FS - 345
+              <br/>
+              Max Pullups - 62
+              <br/>
+              Max Ring MU - 22
+            </dd>
+            <dt>Metcons</dt>
+            <dd>
+              Diane - 1:54
+              <br/>
+              Elizabeth - 3:34
+              <br/>
+              Nancy - 8:10
+              <br/>
+              Fran - 2:17
+              <br/>
+              Grace - 1:36
+              <br/>
+              Isabel - 1:51
+              <br/>
+              Jeremy - 3:37
+              <br/>
+              Russell - 3:44
+            </dd>
+            <dt>Achievements</dt>
+            <dd>
+              2011 NLI Winter Series 1st
+              <br/>
+              2011 South West Fitness Throwdown 2nd
+              <br/>
+              2012 OC Throwdown 6th
+              <br/>
+              2011 CrossFit Games Regionals 6th
+              <br/>
+              2012 CrossFit Sectionals 3rd in World
+              <br/>
+              2012 CrossFit Games SoCal Regionals 2nd
+            </dd>
+          </dl>
+        </div>
+        <div class="well span3">
+          <h3>Sean McChesney</h3>
+          <div class="thumbnail">
+            <img src="<?php bloginfo('template_url'); ?>/images/team-sean-mcchesney.png" width="222" height="222" />
+          </div>
+          <dl>
+            <dt>Name</dd>
+            <dd>Sean McChesney</dd>
+            <dt>PRs</dt>
+            <dd>
+              Clean and Jerk - 300
+              <br/>
+              Snatch - 225
+              <br/>
+              DL - 485
+              <br/>
+              Back Squat - 400
+              <br/>
+              Press - 200
+              <br/>
+              Fran - 2:53
+              <br/>
+              DT - 7:50
+            </dd>
+            <dt>Metcons</dt>
+            <dd>
+              Isabel - 1:56
+              <br/>
+              Murph - 36:28 RX
+            </dd>
+            <dt>Achievements</dt>
+            <dd>
+              2010 Fireman Olympics 9th
+              <br/>
+              2011 Fireman Olympics 2nd
+              <br/>
+              2011 UTC Challenge 7th advanced
+              <br/>
+              2011 Frogman 4th advanced
+              <br/>
+              2011 NLI 9th Advanced
+              <br/>
+              2011 NLI Series 1 9th advanced
+              <br/>
+              2011 NLI Series 2 8th advanced
+              <br/>
+              2012 CrossFit open 56th SoCal
+              <br/>
+              2012 SoCal Regional 27th
+            </dd>
+          </dl>
+        </div>
+        <div class="well span3">
+          <h3>Lauren Gibbs</h3>
+          <div class="thumbnail">
+            <img src="<?php bloginfo('template_url'); ?>/images/team-lauren-gibbs.png" width="222" height="222" />
+          </div>
+          <dl>
+            <dt>Name</dd>
+            <dd>Lauren Gibbs</dd>
+            <dt>PRs</dd>
+            <dd>
+              Dead lift - 400
+              <br/>
+              Back squat - 290
+              <br/>
+              Bench Press - 150
+              <br/>
+              Clean and jerk - 185
+              <br/>
+              Snatch - 155
+              <br/>
+              Front Squat - 225
+            </dd>
+            <dt>Metcons</dt>
+            <dd>
+              Fran - 3:58
+              <br/>
+              Karen - 6:41
+              <br/>
+              Grace - 2:50
+            </dd>
+          </dl>
+        </div>
+        <div class="well span3">
+          <h3>Vince Kong</h3>
+          <div class="thumbnail">
+            <img src="<?php bloginfo('template_url'); ?>/images/team-vince-kong.png" width="222" height="222" />
+          </div>
+          <dl>
+            <dt>Name</dd>
+            <dd>Vince Kong</dd>
+            <dt>PRs</dt>
+            <dd>
+              Snatch - 235
+              <br/>
+              Clean and Jerk - 295
+              <br/>
+              Backsquat 425
+              <br/>
+              Strict press - 225
+              <br/>
+              Deadlift - 505
+            </dd>
+            <dt>Metcons</dt>
+            <dd>
+              Fran - 2:12
+              <br/>
+              Helen - 8:05
+              <br/>
+              Grace - 1:37
+              <br/>
+              DT - 8:23
+              <br/>
+              Nate - 18 rds
+            </dd>
+            <dt>Achievements:</dt>
+            <dd>
+              2011 CrossFit Games Open - 63rd
+              <br/>
+              2012 CrossFit Games Open - 29th
+              <br/>
+              2012 CrossFit Games Regionals - Team 10th 
+            </dd>
+            <dt>Other</dt>
+            <dd>
+              Favorite WOD: Rogues "Miagi"
+              Favorite Lift: Snatch
+            </dd>
+          </dl>
+        </div>
+      </div>
+      <div class="row-fluid">
+        <div class="well span3">
+          <h3>Vanessa Reyes</h3>
+          <div class="thumbnail">
+            <img src="<?php bloginfo('template_url'); ?>/images/team-vanessa-reyes.png" width="222" height="222" />
+          </div>
+          <dl>
+            <dt>Name</dd>
+            <dd>Vanessa Reyes</dd>
+            <dt>Stats</dt>
+            <dd>
+              135lb
+              <br/>
+              5'0''
+              <br/>
+              23 years old
+            </dd>
+            <dt>PRs</dt>
+            <dd>
+              Clean and Jerk - 165
+              <br/>
+              Overhead squat - 135
+              <br/>
+              Snatch - 135
+              <br/>
+              Deadlift - 300
+              <br/>
+              Squat - 245
+            </dd>
+            <dt>Metcons</dt>
+            <dd>
+              Fran - 3:43
+              <br/>
+              Diane - 5:10
+              <br/>
+              Mary - 10 rounds + 4 pull ups
+              <br/>
+              Helen - 9:45
+              <br/>
+              Grace - 2:05
+            </dd>
+            <dt>Other</dt>
+            <dd>
+              Crossfitting since Jan 2012
+              <br/>
+              Ended 2012 So Cal open #32
+              <br/>
+              Competed in 2012 So Cal Regionals, only Day 1. End of day one 25th out of 38. Got married the next day :)
+            </dd>
           </dl>
         </div>
         <div class="well span3">
@@ -30,110 +263,26 @@
           <dl>
             <dt>Name</dd>
             <dd>Melissa Nelson</dd>
-            <dt>Age</dd>
-            <dd>22</dd>
-            <dt>Years competing</dd>
-            <dd>10</dd>
-            <dt>Favorite workout</dd>
-            <dd>Fran</dd>
-          </dl>
-        </div>
-        <div class="well span3">
-          <h3>Matt Banwart</h3>
-          <div class="thumbnail">
-            <img src="<?php bloginfo('template_url'); ?>/images/team-kenny-leverich.png" width="222" height="222" />
-          </div>
-          <dl>
-            <dt>Name</dd>
-            <dd>Matt Banwart</dd>
-            <dt>Age</dd>
-            <dd>22</dd>
-            <dt>Years competing</dd>
-            <dd>10</dd>
-            <dt>Favorite workout</dd>
-            <dd>Fran</dd>
-          </dl>
-        </div>
-        <div class="well span3">
-          <h3>Andrew Kong</h3>
-          <div class="thumbnail">
-            <img src="<?php bloginfo('template_url'); ?>/images/team-kenny-leverich.png" width="222" height="222" />
-          </div>
-          <dl>
-            <dt>Name</dd>
-            <dd>Andrew Kong</dd>
-            <dt>Age</dd>
-            <dd>22</dd>
-            <dt>Years competing</dd>
-            <dd>10</dd>
-            <dt>Favorite workout</dd>
-            <dd>Fran</dd>
-          </dl>
-        </div>
-      </div>
-      <div class="row-fluid hide">
-        <div class="well span3">
-          <h3>Andrew Kong</h3>
-          <div class="thumbnail">
-            <img src="<?php bloginfo('template_url'); ?>/images/team-kenny-leverich.png" width="222" height="222" />
-          </div>
-          <dl>
-            <dt>Name</dd>
-            <dd>Andrew Kong</dd>
-            <dt>Age</dd>
-            <dd>22</dd>
-            <dt>Years competing</dd>
-            <dd>10</dd>
-            <dt>Favorite workout</dd>
-            <dd>Fran</dd>
-          </dl>
-        </div>
-        <div class="well span3">
-          <h3>Andrew Kong</h3>
-          <div class="thumbnail">
-            <img src="<?php bloginfo('template_url'); ?>/images/team-kenny-leverich.png" width="222" height="222" />
-          </div>
-          <dl>
-            <dt>Name</dd>
-            <dd>Andrew Kong</dd>
-            <dt>Age</dd>
-            <dd>22</dd>
-            <dt>Years competing</dd>
-            <dd>10</dd>
-            <dt>Favorite workout</dd>
-            <dd>Fran</dd>
-          </dl>
-        </div>
-        <div class="well span3">
-          <h3>Andrew Kong</h3>
-          <div class="thumbnail">
-            <img src="<?php bloginfo('template_url'); ?>/images/team-kenny-leverich.png" width="222" height="222" />
-          </div>
-          <dl>
-            <dt>Name</dd>
-            <dd>Andrew Kong</dd>
-            <dt>Age</dd>
-            <dd>22</dd>
-            <dt>Years competing</dd>
-            <dd>10</dd>
-            <dt>Favorite workout</dd>
-            <dd>Fran</dd>
-          </dl>
-        </div>
-        <div class="well span3">
-          <h3>Andrew Kong</h3>
-          <div class="thumbnail">
-            <img src="<?php bloginfo('template_url'); ?>/images/team-kenny-leverich.png" width="222" height="222" />
-          </div>
-          <dl>
-            <dt>Name</dd>
-            <dd>Andrew Kong</dd>
-            <dt>Age</dd>
-            <dd>22</dd>
-            <dt>Years competing</dd>
-            <dd>10</dd>
-            <dt>Favorite workout</dd>
-            <dd>Fran</dd>
+            <dt>PRs</dt>
+            <dd>
+              Fran 3:42
+              <br/>
+              Helen - 8:22
+              <br/>
+              Cindy - 20 rds
+              <br/>
+              Elizabeth - 11:10
+              <br/>
+              Deadlift - 300
+              <br/>
+              Back squat - 225
+              <br/>
+              Power clean - 160
+              <br/>
+              Clean &amp; jerk - 145
+              <br/>
+              Overhead squat - 150
+            </dd>
           </dl>
         </div>
       </div>
