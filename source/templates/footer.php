336b2b59d85510f25b912485ffe661ae60dddbc3
Joshua Abbott <joshnabbott@gmail.com>
2012-07-20 11:00:56 -0700

    Adding credit card logos to footer

--- source/templates/footer.php
+++ source/templates/footer.php
@@ -1,5 +1,6 @@
     <footer class="row-fluid">
       <p class="pull-right">&copy; 2012 PHENOM&trade;. All rights reserved. &bull; <a href="/contact">Contact</a> &bull; <a href="/about">About</a> &bull; <a href="/terms-of-use">Terms and Conditions</a> &bull; <a href="/privacy-policy">Privacy Policy</a> &bull; <a href="/returns">Returns</a> &bull; <a href="http://wordpress.org/" rel="generator">Powered by WordPress</a> and a whole lotta love.</p>
+      <div class="pull-right"><img src="<?php bloginfo('template_url'); ?>/images/we-accept-credit-cards.png" alt="We accept all major credit cards and PayPal." /></div>
     </footer>
   </div><!-- end primary-container -->
   <?php wp_footer(); ?>
