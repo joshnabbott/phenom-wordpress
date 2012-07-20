c2b79a11c234853a89579228310cac7b92bdeb27
Joshua Abbott <joshnabbott@gmail.com>
2012-07-20 09:23:48 -0700

    Update footer nav.

--- source/templates/footer.php
+++ source/templates/footer.php
@@ -1,5 +1,5 @@
     <footer class="row-fluid">
-      <p class="pull-right">&copy; 2012 PHENOM&trade;. All rights reserved. &bull; <a href="/terms-of-use">Terms and Conditions</a> &bull; <a href="/privacy-policy">Privacy Policy</a> &bull; <a href="/returns">Returns</a> &bull; <a href="http://wordpress.org/" rel="generator">Powered by WordPress</a> and a whole lotta love.</p>
+      <p class="pull-right">&copy; 2012 PHENOM&trade;. All rights reserved. &bull; <a href="/contact">Contact</a> &bull; <a href="/about">About</a> &bull; <a href="/terms-of-use">Terms and Conditions</a> &bull; <a href="/privacy-policy">Privacy Policy</a> &bull; <a href="/returns">Returns</a> &bull; <a href="http://wordpress.org/" rel="generator">Powered by WordPress</a> and a whole lotta love.</p>
     </footer>
   </div><!-- end primary-container -->
   <?php wp_footer(); ?>
