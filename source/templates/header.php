047d11a79b0e949e0719f4e6505f6d82425cd197
Joshua Abbott <joshnabbott@gmail.com>
2012-07-20 10:42:17 -0700

    Adding mailchimp signup

--- source/templates/header.php
+++ source/templates/header.php
@@ -15,10 +15,25 @@
     <header class="row-fluid">
       <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 
-      <form action="http://shop.phenom.fm/search" method="get" class="form-search pull-right">
-        <input type="text" name="q" placeholder="search products" class="input-medium search-query" />
-        <input type="submit" value="Search" class="btn" />
+      <form action="http://phenom.us5.list-manage2.com/subscribe/post" method="post" class="form-search pull-right">
+        <p>Join the PHENOM family, stay up-to-date with the latest deals and announcements from PHENOM.</p>
+        <input type="text" name="EMAIL" placeholder="youremail@example.com" class="input-medium search-query" />
+        <input type="hidden" name="u" value="7ae6da4a5505c9c2c95abfd9d" />
+        <input type="hidden" name="id" value="504dd42b12" />
+        <input type="submit" value="Subscribe" class="btn" />
       </form>
+      <div class="modal hide" id="signup-message">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">×</button>
+          <h3></h3>
+        </div>
+        <div class="modal-body">
+          <p>It works</p>
+        </div>
+        <div class="modal-footer">
+          <a href="#" class="btn" data-dismiss="modal">Close</a>
+        </div>
+      </div>
 
         <?php
           wp_nav_menu(array(
